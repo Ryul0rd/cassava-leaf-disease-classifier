8a443b47731453acbf3ff107c8f34f452aad5a36
Ryan Slattery <18477649+Ryul0rd@users.noreply.github.com>
2021-09-09 22:34:47 -0700

    Polish make_image_folders.py

--- make_image_folders.py
+++ make_image_folders.py
@@ -3,9 +3,6 @@ import pandas as pd
 import shutil
 
 def main():
-    if not Path('data/train_images').exists():
-        print('No train_images folder to sort!')
-        return
     for i in range(5):
         Path(f'data/{i}').mkdir(exist_ok=True, parents=True)
     df = pd.read_csv('data/train.csv')
@@ -13,6 +10,7 @@ def main():
         source = Path(f'data/train_images/{row["image_id"]}')
         destination = Path(f'data/{row["label"]}')
         shutil.move(source, destination)
+    Path('data/train_images').rmdir()
 
 if __name__ == '__main__':
     main()
