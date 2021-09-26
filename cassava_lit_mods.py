167720279e76164d8e1b06d296b20835d419d71f
Ryan Slattery <18477649+Ryul0rd@users.noreply.github.com>
2021-09-25 20:01:37 -0700

    Finalize training files

--- cassava_lit_mods.py
+++ cassava_lit_mods.py
@@ -1,4 +1,3 @@
-from numpy.core.fromnumeric import transpose
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, WeightedRandomSampler
@@ -14,7 +13,7 @@ import pandas as pd
 
 
 class CassavaDataMod(pl.LightningDataModule):
-    def __init__(self, batch_size=32, val_size=0.2, balance_train=True, balance_val=False):
+    def __init__(self, batch_size=32, val_size=0.2, balance_train=False, balance_val=False):
         super().__init__()
         self.data_dir = 'data/'
         self.batch_size = batch_size
@@ -31,14 +30,14 @@ class CassavaDataMod(pl.LightningDataModule):
             ToTensorV2(),
         ])
 
-    def transform(self, image):
+    def aug_transform(self, image):
         return self.augs(image=np.array(image))['image'] # Dumb glue to accomodate albumentations
 
     def prepare_data(self):
         pass
 
     def setup(self, stage=None):
-        self.data = ImageFolder(self.data_dir, transform=self.transform)
+        self.data = ImageFolder(self.data_dir, transform=self.aug_transform)
 
         # making weighted random samplers to deal with class balance
         df = pd.read_csv('data/train.csv')
@@ -57,8 +56,14 @@ class CassavaDataMod(pl.LightningDataModule):
 
         train_weights = is_train * (weights if self.balance_train else 1)
         val_weights = is_val * (weights if self.balance_val else 1)
-        self.train_sampler = WeightedRandomSampler(train_weights, int(len(self.data)*(1-self.val_size)/8), replacement=True)
-        self.val_sampler = WeightedRandomSampler(val_weights, int(len(self.data)*self.val_size/4), replacement=True)
+        if self.balance_train:
+            self.train_sampler = WeightedRandomSampler(train_weights, int(len(self.data)*(1-self.val_size)), replacement=True)
+        else:
+            self.train_sampler = WeightedRandomSampler(train_weights, int(len(self.data)*(1-self.val_size)), replacement=False)
+        if self.balance_val:
+            self.val_sampler = WeightedRandomSampler(val_weights, int(len(self.data)*self.val_size), replacement=True)
+        else:
+            self.val_sampler = WeightedRandomSampler(val_weights, int(len(self.data)*self.val_size), replacement=False)
 
     def train_dataloader(self):
         return DataLoader(self.data, sampler=self.train_sampler, batch_size=self.batch_size, num_workers=3, pin_memory=True)
@@ -72,40 +77,44 @@ class CassavaDataMod(pl.LightningDataModule):
 
 
 class CassavaLitMod(pl.LightningModule):
-    def __init__(self, lr=1e-3, hidden_size=32, wd=1e-5):
+    def __init__(self, transformer_size='tiny', learning_rate=3e-4, hidden_size=0, weight_decay=1e-5):
         super().__init__()
-        self.lr = lr
-        self.wd = wd
+        self.lr = learning_rate
+        self.wd = weight_decay
         self.num_classes = 5
         self.accuracy = Accuracy()
 
-        self.model = timm.create_model("vit_base_patch16_384", pretrained=True)
+        self.model = timm.create_model(f'vit_{transformer_size}_patch16_384', pretrained=True)
         for param in self.model.parameters():
             param.requires_grad = False
-        self.model.head = nn.Sequential(
-            nn.Linear(self.model.head.in_features, self.num_classes),
-            # nn.Linear(self.model.head.in_features, hidden_size),
-            # nn.ReLU(),
-            # nn.Linear(hidden_size, self.num_classes),
-        )
+        if hidden_size == 0:
+            self.model.head = nn.Linear(self.model.head.in_features, self.num_classes)
+        else:
+            self.model.head = nn.Sequential(
+                nn.Linear(self.model.head.in_features, hidden_size),
+                nn.ReLU(),
+                nn.Linear(hidden_size, self.num_classes),
+            )
+
+        self.save_hyperparameters()
 
     def forward(self, x):
         return self.model(x)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), self.lr, weight_decay=self.wd)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=2)
+        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda epoch: 1.0 / (1.0 + epoch))
         return {
            'optimizer': optimizer,
            'lr_scheduler': scheduler,
-           'monitor': 'val_loss'
+           'monitor': 'val/loss'
         }
 
     def training_step(self, train_batch, batch_idx):
         x, y_true = train_batch
-        y_pred = self.model(x)
-        loss = nn.functional.cross_entropy(y_pred, y_true)
-        self.log('train_loss', loss, on_step=True)
+        preds = self.model(x)
+        loss = nn.functional.cross_entropy(preds, y_true)
+        self.log('train/loss', loss, on_step=True)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
@@ -113,8 +122,8 @@ class CassavaLitMod(pl.LightningModule):
         preds = self.model(x)
         loss = nn.functional.cross_entropy(preds, y_true)
         acc = self.accuracy(torch.argmax(preds, dim=1), y_true)
-        self.log('val_loss', loss, prog_bar=True)
-        self.log('accuracy', acc, prog_bar=True)
+        self.log('val/loss', loss, prog_bar=True)
+        self.log('val/accuracy', acc, prog_bar=True)
         return preds, y_true
 
     def validation_epoch_end(self, validation_step_outputs):
--- train.py
+++ train.py
@@ -1,19 +1,23 @@
 import wandb
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
+from pytorch_lightning.callbacks import LearningRateMonitor
 from cassava_lit_mods import CassavaLitMod, CassavaDataMod
 
 def main():
     logger = WandbLogger(project='cassava-leaf-disease-classifier', log_model=False, config=wandb.config)
     config = logger.experiment.config
     callbacks = [
-        #EarlyStopping('val_loss', patience=5),
         LearningRateMonitor(logging_interval='step'),
         ]
     trainer = Trainer(max_epochs=config['epochs'], logger=logger, callbacks=callbacks, gpus=1)
-    model = CassavaLitMod(lr=config['learning_rate'], hidden_size=config['hidden_size'], wd=config['weight_decay'])
-    data = CassavaDataMod(batch_size=config['batch_size'])
+    model = CassavaLitMod(
+        transformer_size=config['transformer_size'],
+        learning_rate=config['learning_rate'],
+        hidden_size=config['hidden_size'],
+        weight_decay=config['weight_decay']
+        )
+    data = CassavaDataMod(batch_size=config['batch_size'], val_size=config['val_size'])
     trainer.fit(model, data)
 
 if __name__ == '__main__':
